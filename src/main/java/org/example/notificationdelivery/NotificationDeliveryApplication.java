3dadac4fe8da7ee0b757ba027212d2136cd4397e
fore1k4 <vanyushagtr270@mail.ru>
2025-09-14 10:08:50 +0500

    Initial commit

---
+++ src/main/java/org/example/notificationdelivery/NotificationDeliveryApplication.java
@@ -0,0 +1,13 @@
+package org.example.notificationdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationDeliveryApplication.class, args);
+    }
+
+}
